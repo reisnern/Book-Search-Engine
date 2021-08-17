df717df9f8e333b370e09006438fbc4d08d1644c
Nick Reisner <nreisne@wgu.edu>
2021-08-17 06:39:20 -0500

    adding to resolvers

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,3 +1,14 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+          return userData;
+        }
+
+    throw new AuthenticationError('You are not logged in please try again')
