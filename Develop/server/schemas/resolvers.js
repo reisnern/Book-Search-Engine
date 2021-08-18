42a087b4c5452bd6b7faeb838481a97a571f1c72
Nick Reisner <nreisne@wgu.edu>
2021-08-17 19:38:20 -0500

    adding to resolvers.js

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -16,7 +16,34 @@ const resolvers = {
     users: async () => {
         return User.find()
             .select('-__v -password')
-    }
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+    },
 
-  }
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          res.send('Incorrect credentials');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          res.send('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+        return { token, user };
+      }
+    }
 };
